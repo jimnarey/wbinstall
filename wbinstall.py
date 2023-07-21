97b968c9219fa789b42963a596b438710198b371
Jim Narey <jimnarey@me.com>
2023-07-21 16:43:36 +0100

    Open image files

--- wbinstall.py
+++ wbinstall.py
@@ -6,6 +6,7 @@ from amitools.fs.FSString import FSString
 from amitools.fs.rdb.RDisk import RDisk
 from amitools.fs.blkdev.BlkDevFactory import BlkDevFactory 
 from amitools.fs.blkdev.BlockDevice import BlockDevice
+from amitools.fs.blkdev.HDFBlockDevice import HDFBlockDevice
 from amitools.fs.blkdev.PartBlockDevice import PartBlockDevice 
 from amitools.fs.blkdev.RawBlockDevice import RawBlockDevice
 from amitools.fs.ADFSVolume import ADFSVolume
@@ -15,83 +16,63 @@ from amitoolhelpers.disk_geometry import AutoDiskGeometry
 
 load_dotenv()
 
-# bdf = BlkDevFactory.BlkDevFactory()
-
-# a = bdf.open(os.environ.get('ADF_PATH'), read_only=True)
-# b = bdf.open(os.environ.get('HDF_PATH'), read_only=True)
-
-RDB_IMG_PATH = os.path.join(os.environ.get('BASE_DIR'), 'testing', 'a600-2.1-workbench-rdb-3gb.hdf')
-
-
 def open_rdb(path: str, read_only: bool=True) -> Any:
-    bs = 512
+    bs: int = 512
     rawdev = RawBlockDevice(path, read_only, block_bytes=bs)
     rawdev.open()
-    # check block size stored in rdb
     rdisk = RDisk(rawdev)
-    rdb_bs = rdisk.peek_block_size()
+    rdb_bs = int(rdisk.peek_block_size()) # type: ignore
     if rdb_bs != bs:
-        # adjust block size and re-open
         rawdev.close()
-        bs = rdb_bs
-        rawdev = RawBlockDevice(path, read_only, block_bytes=bs)
+        rawdev = RawBlockDevice(path, read_only, block_bytes=rdb_bs)
         rawdev.open()
         rdisk = RDisk(rawdev)
     if not rdisk.open():
-        raise IOError("can't open rdisk of image file")
+        raise IOError("Error opening rdisk")
     return rdisk
 
 
-def create_rdisk(path, mbs):
-    if mbs < 16:
-        return
+def create_raw_block_device(path: str, mbs: int) -> RawBlockDevice:
     geo = AutoDiskGeometry(mbs)
     blkdev = RawBlockDevice(path, block_bytes=geo.block_bytes)
     blkdev.create(geo.get_num_blocks())
-    blkdev.geo = geo
-    # return RDisk(blkdev)
+    blkdev.geo = geo # type: ignore
+    return blkdev
+
+def create_rdisk(blkdev: BlockDevice) -> RDisk:
     rdisk = RDisk(blkdev)
-    rdisk.create(geo)
+    rdisk.create(blkdev.geo) # type: ignore
     return rdisk
 
-def create_partition(rdisk: RDisk):
-    drive_name = FSString('DH0')
+def create_partition(rdisk: RDisk, name='DH0') -> PartBlockDevice:
+    drive_name = FSString(name)
     rdisk.add_partition(drive_name, rdisk.get_cyl_range())
+    blkdev = PartBlockDevice(r.rawblk, rdisk.parts[0].part_blk)
+    blkdev.open()
+    return blkdev
+     
+def create_hdf(path, mbs) -> HDFBlockDevice:
+    geo = AutoDiskGeometry(mbs)
+    blkdev = HDFBlockDevice(path)
+    blkdev.create(geo)
+    blkdev.geo = geo
+    return blkdev
 
-def open_img(path: str, read_only: bool=True) -> BlockDevice:
+def create_volume(blkdev: BlockDevice, name: str='Worbench', is_ffs: bool=True) -> ADFSVolume:
+    vol = ADFSVolume(blkdev)
+    vol.create(FSString(name), is_ffs=is_ffs)
+    return vol
+
+def open_image_file(path: str, read_only: bool=True) -> ADFSVolume:
     bdf = BlkDevFactory()
-    blk = bdf.open(path, read_only=read_only)
-    return blk, ADFSVolume(blk)
-
+    blkdev = bdf.open(path, read_only=read_only)
+    return ADFSVolume(blkdev)
 
 def test_vals(max):
     for i in range(max):
-        # print(i)
         adg = AutoDiskGeometry(i)
-        # adg.print()
-        # print(bool(adg.size))
-        # adg.print()
 
-# rdb = open_rdb(RDB_IMG_PATH)
 
-# badf, vadf = open_img(os.environ.get('ADF_PATH'))
-# bhdf, vhdf = open_img(os.environ.get('HDF_PATH'))
-# brdb, vrdb = open_img(RDB_IMG_PATH)
-
-r = create_rdisk('test.img', 20)
-create_partition(r)
-
-p: PartBlockDevice = r.parts[0]
-pbd = PartBlockDevice(r.rawblk, p.part_blk)
-pbd.open()
-v = ADFSVolume(pbd)
-v.create(FSString('System'), is_ffs=True)
-print(v.get_info())
-
-# r.close()
-
-# p = BlkDevFactory().open('test.img')
-
-# v = ADFSVolume(p)
-
-# v.create(FSString('System'), is_ffs=True)
+adfv = open_image_file(os.environ.get('ADF_PATH'))
+adfv.open()
+print(adfv.get_info())
