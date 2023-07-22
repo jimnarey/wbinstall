180e5e4e443fd9336b66aa2c5d0ba246fd16645f
Jim Narey <jimnarey@me.com>
2023-07-22 14:24:29 +0100

    Add tree to VolumeTree

--- amitoolhelpers/volume_tree.py
+++ amitoolhelpers/volume_tree.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+import os
 from collections.abc import Iterable
 
 from amitools.fs.ADFSDir import ADFSDir
+from amitools.fs.ADFSFile import ADFSFile
 from amitools.fs.blkdev.BlockDevice import BlockDevice
 from amitools.fs.ADFSVolume import ADFSVolume
 
@@ -15,15 +17,37 @@ class VolumeTree:
         self.volume.open()
         self.volume.root_dir.read(recursive=True)
         self.entries = self.volume.root_dir.entries
-        self.flat_entries = [item for item in self.flatten(self.entries)]
-        # self.files = [item for item in self.flat_entries if ]
+        # self.flat_entries = [item for item in self.flatten(self.entries)]
+        self.flat_entries = []
+        self.paths = [item.name.get_unicode_name() for item in self.flat_entries]
+        self.files = [item for item in self.flat_entries if isinstance(item, ADFSFile)]
+        self.dirs = [item for item in self.flat_entries if isinstance(item, ADFSDir)]
+        self.tree = {'name': self.volume.name.get_unicode(), 'entries': []}
+        self.create_tree(self.entries, self.tree)
 
 
-    def flatten(self, root):
+    # def flatten(self, root):
+    #     for entry in root:
+    #         if isinstance(entry, ADFSDir):
+    #             entry.read()
+    #             yield from self.flatten(entry.entries)
+    #         print(entry)
+    #         yield entry
+
+    def create_tree(self, root, parent):
         for entry in root:
+            tree_entry = {'name': entry.name.get_unicode_name(), 'path': os.path.join(*entry.get_node_path())}
             if isinstance(entry, ADFSDir):
                 entry.read()
-                yield from self.flatten(entry.entries)
-            else:
-                print(entry)
-                yield entry
+                tree_entry['entries'] = []
+                self.create_tree(entry.entries, tree_entry)
+            self.flat_entries.append(entry)
+            parent['entries'].append(tree_entry)
+
+    # def item_from_path(self, path):
+
+
+    # def paths(self):
+    #     for item in self.flat_entries:
+    #         path = os.path.join(*item.get_node_path())
+    #         print(path)
