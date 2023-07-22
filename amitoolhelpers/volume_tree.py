7df59093499958b8df2a914f1921e445441b7596
Jim Narey <jimnarey@me.com>
2023-07-22 14:48:50 +0100

    Fix errors and tidy VolumeTree

--- amitoolhelpers/volume_tree.py
+++ amitoolhelpers/volume_tree.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import hashlib
 from collections.abc import Iterable
 
 from amitools.fs.ADFSDir import ADFSDir
@@ -17,37 +18,33 @@ class VolumeTree:
         self.volume.open()
         self.volume.root_dir.read(recursive=True)
         self.entries = self.volume.root_dir.entries
-        # self.flat_entries = [item for item in self.flatten(self.entries)]
         self.flat_entries = []
-        self.paths = [item.name.get_unicode_name() for item in self.flat_entries]
-        self.files = [item for item in self.flat_entries if isinstance(item, ADFSFile)]
-        self.dirs = [item for item in self.flat_entries if isinstance(item, ADFSDir)]
-        self.tree = {'name': self.volume.name.get_unicode(), 'entries': []}
+        self.hashes = {}
+        self.paths = {}
+        self.tree = {'name': self.volume.name.get_unicode(), 'entries': []}        
         self.create_tree(self.entries, self.tree)
-
-
-    # def flatten(self, root):
-    #     for entry in root:
-    #         if isinstance(entry, ADFSDir):
-    #             entry.read()
-    #             yield from self.flatten(entry.entries)
-    #         print(entry)
-    #         yield entry
+        
 
     def create_tree(self, root, parent):
+        hash = hashlib.sha256()
         for entry in root:
-            tree_entry = {'name': entry.name.get_unicode_name(), 'path': os.path.join(*entry.get_node_path())}
-            if isinstance(entry, ADFSDir):
-                entry.read()
+            path = os.path.join(*entry.get_node_path())
+            tree_entry = {'name': entry.name.get_unicode_name(), 'path': path}
+            entry.read()
+            if isinstance(entry, ADFSDir):    
                 tree_entry['entries'] = []
                 self.create_tree(entry.entries, tree_entry)
+            else:
+                hash.update(entry.data)
+                digest = hash.hexdigest()
+                tree_entry['sha256'] = digest
+                self.hashes[digest] = entry
+            self.paths[path] = entry
             self.flat_entries.append(entry)
             parent['entries'].append(tree_entry)
 
-    # def item_from_path(self, path):
-
-
-    # def paths(self):
-    #     for item in self.flat_entries:
-    #         path = os.path.join(*item.get_node_path())
-    #         print(path)
+    def dirs(self):
+        return [item for item in self.flat_entries if isinstance(item, ADFSDir)]
+    
+    def files(self):
+        return [item for item in self.flat_entries if isinstance(item, ADFSFile)]
